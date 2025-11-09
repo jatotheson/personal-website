aef586db26e725dce9040ca529689089fad5ca15
Jason Chung <jason6858@gmail.com>
2025-11-09 15:55:23 -0700

    fix dependency

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  // base: '/personal-website/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
